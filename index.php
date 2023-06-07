710e00e9e50d58903f7bb05a6d8eaf8df4ebb698
Vojtěch Kuchař <vojta.kuchar.zssob@seznam.cz>
2023-06-07 19:26:03 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <title>Galerie obrázků</title>
+    <style>
+        .image-gallery {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+
+        .image-gallery img {
+            max-width: 80%;
+            max-height: 80%;
+        }
+    </style>
+</head>
+<body>
+<div class="image-gallery">
+    <?php
+
+
+    // Získání seznamu souborů v dané složce
+    $obrazky = glob(dirname(realpath(__FILE__)).'\\pictures\\' . '*.{jpg,jpeg,png,gif}', GLOB_BRACE);
+    // Získání aktuálního indexu obrázku z URL parametru
+    $aktualniIndex = isset($_GET['index']) ? intval($_GET['index']) : 0;
+    $pocetObrazku = count($obrazky);
+
+    // Určení indexů předchozího a následujícího obrázku
+    if ($aktualniIndex > 0) {
+        $predchoziIndex = $aktualniIndex - 1;
+    } else {
+        $predchoziIndex = $pocetObrazku - 1;
+    }
+
+    $nasledujiciIndex = ($aktualniIndex < $pocetObrazku - 1) ? $aktualniIndex + 1 : 0;
+
+    // Odstranění všeho před slovem "pictures" v cestě
+    $novySoubor = substr($obrazky[$aktualniIndex], strpos($obrazky[$aktualniIndex], 'pictures'));
+    // Zobrazení aktuálního obrázku
+    echo '<img src="' . htmlspecialchars($novySoubor) . '" alt="Obrázek"><br>';
+
+    // Odkazy na předchozí a následující obrázek
+    echo '<a href="?index=' . $predchoziIndex . '">&#8592; Next</a>';
+    echo ' | ';
+    echo '<a href="?index=' . $nasledujiciIndex . '">Previous &#8594;</a>';
+
+    ?>
+
+</div>
+</body>
+</html>
